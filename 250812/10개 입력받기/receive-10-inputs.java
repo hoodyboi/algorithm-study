991ed239e656638c25a2ec3909581bc5011eee19
hoodyboi <93381300+hoodyboi@users.noreply.github.com>
2025-08-12 08:47:41 +0900

    Title: 10개 입력받기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        for(int i =0; i<10; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+        int cnt = 0;
+
+        for(int i = 0; i<10; i++){
+            if(arr[i] != 0){
+                sum += arr[i];
+                cnt++;
+            } else {
+                break;
+            }
+        }
+        System.out.printf("%d %.1f", sum, (float)sum/cnt);
+    }
+}
